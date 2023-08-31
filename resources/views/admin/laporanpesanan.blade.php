5f5e10183e2dba40be66d55042665d308da84563
portgazdbeel <portgaz77@gmail.com>
2023-08-31 12:14:29 +0700

    commit

--- resources/views/admin/laporanpesanan.blade.php
+++ resources/views/admin/laporanpesanan.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laporan Pembelian</title>
+    <title>Laporan Penjualan</title>
     <!-- Fonts -->
 
     <!-- Styles -->
